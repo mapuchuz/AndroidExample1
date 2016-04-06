e4ea4ed9f3c85616058f1cdbf9cf50167eaa4913
Vincent Demolombe <vincent.demolombe@sfr.fr>
2016-04-06 12:54:38 +0200

    avec draxer de merde

--- src/com/example/salut/PalindromeActivity.java
+++ src/com/example/salut/PalindromeActivity.java
@@ -1,15 +1,36 @@
 package com.example.salut;
 
+import java.util.Locale;
+
 import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class PalindromeActivity extends Activity {
+	
+	  private ListView mDrawerList;
+	    private DrawerLayout mDrawerLayout;
+	    private ActionBarDrawerToggle mDrawerToggle;
+
+	    private CharSequence mDrawerTitle;
+	    private CharSequence mTitle;
+	    private String[] mPlanetTitles;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +39,46 @@ public class PalindromeActivity extends Activity {
 		
 		EditText monTexte = (EditText) findViewById(R.id.textSource);
 		monTexte.addTextChangedListener( mTextListener );
+		
+        mTitle = mDrawerTitle = getTitle();
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_menu_black_24dp,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+                ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
 	}
 
 	@Override
@@ -57,4 +118,74 @@ public class PalindromeActivity extends Activity {
 		}
 	};
 
+	
+	  /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+    	if (position == 0) {
+			Intent intent = new Intent(this, PiedActivity.class);
+			startActivity(intent);
+		}
+
+      	if (position ==1) {
+      				Intent intent = new Intent(this, ContactsActivity.class);
+			startActivity(intent);
+		}
+     	if (position ==2) {
+			Intent intent = new Intent(this, VideoActivity.class);
+			startActivity(intent);
+		}
+     	if (position ==3) {
+			Intent intent = new Intent(this, PalindromeActivity.class);
+			startActivity(intent);
+		}
+	
+
+    	
+    	// update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+ 
+
+    /**
+     * Fragment that appears in the "content_frame", shows a planet
+     */
+    public static class PlanetFragment extends Fragment {
+        public static final String ARG_PLANET_NUMBER = "planet_number";
+
+        public PlanetFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+            int i = getArguments().getInt(ARG_PLANET_NUMBER);
+            String planet = getResources().getStringArray(R.array.planets_array)[i];
+
+            int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
+                            "drawable", getActivity().getPackageName());
+            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+            getActivity().setTitle(planet);
+            return rootView;
+        }
+    }
+	
 }
